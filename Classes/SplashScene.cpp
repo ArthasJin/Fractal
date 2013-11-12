713c565bb1919ae3e7a5d5812925594a58935b72
Xiaolei Jin <xiaoleij@andrew.cmu.edu>
2013-11-12 01:25:18 -0800

    Add SpashScene

---
+++ Classes/SplashScene.cpp
@@ -0,0 +1,18 @@
+/*
+ * SplashScene.cpp
+ *
+ *  Created on: Nov 8, 2013
+ *      Author: xiaolei
+ */
+
+#include "SplashScene.h"
+
+SplashScene::SplashScene() {
+  // TODO Auto-generated constructor stub
+
+}
+
+SplashScene::~SplashScene() {
+  // TODO Auto-generated destructor stub
+}
+
---
+++ Classes/SplashScene.h
@@ -0,0 +1,17 @@
+/*
+ * SplashScene.h
+ *
+ *  Created on: Nov 8, 2013
+ *      Author: xiaolei
+ */
+
+#ifndef SPLASHSCENE_H_
+#define SPLASHSCENE_H_
+
+class SplashScene : public cocos2d::CCLayer {
+ public:
+  SplashScene();
+  virtual ~SplashScene();
+};
+
+#endif /* SPLASHSCENE_H_ */
